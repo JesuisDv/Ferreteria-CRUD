1357ef4f14e228061f61bf051a3978565e5f9a58
JesuisDv <jdfrancov2020@gmail.com>
2025-05-17 18:57:59 -0500

    Implementa la funcionalidad de edición en el formulario de productos y corrige la gestión de identificadores.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 let identificador = 0;
 let usuarios = [];
+let editando = null;
+const formulario = document.querySelector("form");
+const tablaBody = document.querySelector("table tbody");
 
-const formulario = document.querySelector("form")
-const tablaBody = document.querySelector("table tbody")
 
 formulario.addEventListener("submit", function(e){
     e.preventDefault()
@@ -12,8 +13,23 @@ formulario.addEventListener("submit", function(e){
     const categoria = document.getElementById("categoria").value;
     const precio = document.getElementById("precio").value; 
 
+    
+
     if(producto && referencia && cantidad){
-        usuarios.push({producto, referencia:Number(referencia), cantidad, categoria, precio});
+        if(editando !==  null){
+            usuarios[editando] = {
+                ...usuarios[editando],
+                producto,
+                referencia: Number(referencia),
+                cantidad,
+                categoria,
+                precio
+
+            };
+            editando = null
+        } else {
+            usuarios.push({id: identificador++, producto, referencia:Number(referencia), cantidad, categoria, precio});
+        }
         this.reset();
         mostrarUsuarios()
     }
@@ -30,7 +46,7 @@ function mostrarUsuarios(){
         fila.innerHTML=``
 
         fila.innerHTML=`
-        <td>${(++identificador).toString().padStart(2, "0")}</td>
+        <td>${usuario.id.toString().padStart(2, "0")}</td>
         <td>${usuario.producto}</td>
         <td>${usuario.referencia}</td>
         <td>${usuario.cantidad}</td>
@@ -39,7 +55,7 @@ function mostrarUsuarios(){
         <td>
             <div class="accionesBtns">
                 <button class="eliminar" onclick="eliminarUsuario(${index})">Eliminar</button>
-                <button class="editar" onclick="">Editar</button>
+                <button class="editar" onclick="editarUsuario(${index})">Editar</button>
             </div>
         </td>
         `
@@ -53,5 +69,19 @@ function eliminarUsuario(index){
 }
 
 function editarUsuario(index){
+    const usuario = usuarios[index];
+    document.getElementById("producto").value = usuario.producto;
+    document.getElementById("producto").value = usuario.producto;
+    document.getElementById("referencia").value = usuario.referencia;
+    document.getElementById("cantidad").value = usuario.cantidad;
+    document.getElementById("categoria").value = usuario.categoria;
+    document.getElementById("precio").value = usuario.precio;
+    editando = index;
     
+    const editarBtn = document.querySelectorAll(".editar");
+
+    editarBtn.forEach((btn, i)=> {
+        btn.style.background = i === index ? "black" : "";
+        btn.style.color = i === index ? "white" : "";
+    });
 };
